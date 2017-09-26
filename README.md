b1d1754c5b50e743b3251a708e451ddb93b225a3
Suthakar <suthakar@1bank.dbs.com>
2017-09-26 15:07:27 +0800

    RestAPIs with Swagger API Documentation

---
+++ README.md
@@ -0,0 +1,28 @@
+
+== To Build from Source
+
+	$ mvn clean install
+
+== To Run Application
+
+	$ mvn spring-boot:run
+	
+== Sample REST APIs
+
+	http://localhost:8080/SpringBootRestApi/api/user/
+	
+== Swagger API DocumentationLink
+
+   http://localhost:8080/SpringBootRestApi/swagger-ui.html#/api
+	
+== To Change Configurations
+
+	SpringBootRestApiSwagger\src\main\resources\application.yml
+
+== For Comments, Issues and Bug Reporting Contact suthakarkb@gmail.com
+	
+	<Suthakar Kumar>suthakarkb@gmail.com
+
+== License
+Spring Boot is Open Source software released under the
+http://www.apache.org/licenses/LICENSE-2.0.html[Apache 2.0 license].
---
+++ src/main/java/com/websystique/springboot/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.websystique.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+
+import springfox.documentation.builders.*;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.swagger.web.*;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {     
+
+    ApiInfo apiInfo() {
+      return new ApiInfoBuilder()
+        .title("API Reference")
+        .version("1.0.0")
+        .build();
+    }
+	
+    @Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.any())              
+          .paths(PathSelectors.any())                          
+          .build();                                           
+    }
+}
